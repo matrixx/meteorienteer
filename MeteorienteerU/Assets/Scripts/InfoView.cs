7bfde7014800d7599a7c743ca57af075326b5747
Anjuli Pullinen <anjuli.pullinen@uta.fi>
2013-04-19 20:36:51 +0300

    adding new files

---
+++ MeteorienteerU/Assets/Scripts/InfoView.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class InfoView : MonoBehaviour {
+	
+	private MainMenu mainMenu;
+	
+	void Awake()
+	{
+		mainMenu = GetComponent<MainMenu>();
+	}
+	
+	void OnGUI()
+	{
+		GUI.skin = GUIOptions.Singleton.appStyle;
+		GUILayout.BeginArea(new Rect(0,0,Screen.width, Screen.height));
+		GUILayout.BeginVertical();
+		GUILayout.Label(Loc.Str("info_text"));
+		GUILayout.FlexibleSpace();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button(Loc.Str("infoview_back")))
+		{
+			this.enabled = false;
+			mainMenu.enabled = true;
+		}
+		GUILayout.FlexibleSpace();
+		GUILayout.Button(Loc.Str("infoview_"));
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal();
+		
+		GUILayout.FlexibleSpace();
+		GUILayout.Label("");
+		GUILayout.EndVertical();
+		GUILayout.EndArea();
+	}
+	
+}
